6e956464e368d40ad64856c889c96a84e0435fd5
Pratham <gangardepatil643@gmail.com>
2026-05-01 15:23:56 +0530

    add timestamp for ci cd demo

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -9,6 +9,9 @@
 <body>
     <div class="container mt-5">
         <h1 class="mb-4">Employee Management</h1>
+        <h3 style="color:green;">
+🚀 Auto Deployed at: {{ now() }}
+</h3>
         
         @if(session('success'))
             <div class="alert alert-success">
@@ -35,7 +38,6 @@
                         <td>{{ $employee->name }}</td>
                         <td>{{ $employee->created_at }}</td>
                         <td>{{ $employee->updated_at }}</td>
-                        <h2>Deployed at: {{ now() }}</h2>
                         <td>
                             <a href="{{ route('employees.show', $employee->id) }}" class="btn btn-info btn-sm">View</a>
                             <a href="{{ route('employees.edit', $employee->id) }}" class="btn btn-warning btn-sm">Edit</a>
