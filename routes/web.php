6aa582db6009a7cc4414161dc1a00748fde23695
Pratham <gangardepatil643@gmail.com>
2026-05-04 13:40:53 +0530

    web.php updated root url

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/employees');
 });
 
 Route::resource('employees', EmployeeController::class);
